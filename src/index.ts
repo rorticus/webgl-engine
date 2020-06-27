35bf610850e51f34037a0f34f0b8a2321a446c69
Rory Mulligan <rorticus@gmail.com>
2020-06-27 06:30:04 -0400

    Adding index file to rexport

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export * from './Engine';
+export * from './Camera';
+export * from './GameObject';
+export * from './Light';
+export * from './Scene';
+
+export * from './services/MouseService';
+
+export * from './cameras/OrbitCamera';
+
+export * from './animation/AnimationStateMachine';
+export * from './animation/AnimationChannel';
+export * from './animation/RotationAnimationChannel';
+export * from './animation/ScaleAnimationChannel';
+export * from './animation/TranslationAnimationChannel';
